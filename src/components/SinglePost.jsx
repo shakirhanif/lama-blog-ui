1a250c949f4137e0a09d97f52bfe69efab6ffa29
Shakir Hanif <shakirhanif@hotmail.com>
2023-02-05 16:18:54 +0500

    post edit finish

--- src/components/SinglePost.jsx
+++ src/components/SinglePost.jsx
@@ -10,10 +10,15 @@ const SinglePost = () => {
   const location = useLocation();
   const postId = location.pathname.split("/")[2];
   const [post, setPost] = useState({});
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [updateMode, setUpdateMode] = useState(false);
   useEffect(() => {
     const fetchPost = async (Id) => {
       const data = await getSinglePost(Id);
       setPost(data);
+      setTitle(data.title);
+      setDesc(data.desc);
     };
     fetchPost(postId);
   }, [postId]);
@@ -29,6 +34,20 @@ const SinglePost = () => {
       console.log(err.message);
     }
   };
+  const updateHandler = async () => {
+    try {
+      await axios.put(`${url}/post/update/${postId}`, {
+        username: user.username,
+        title,
+        desc,
+      });
+      // window.location.reload();
+      setUpdateMode(false);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
   return (
     <div>
       <div className=" p-[20px] pr-0">
@@ -45,19 +64,34 @@ const SinglePost = () => {
             className="w-[100%] h-[300px] rounded-[5px] object-cover"
           />
         )}
+        {updateMode ? (
+          <div className="mt-[5px] w-[100%]">
+            <input
+              type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              autoFocus
+              className="w-[100%] font-['Lora',serif] text-[28px] text-center focus:outline-none text-gray-600"
+            />
+          </div>
+        ) : (
+          <h1 className=" text-center font-['Lora',serif] text-[28px]">
+            {title}
+            {post?.username === user?.username ? (
+              <div className=" float-right text-[16px]">
+                <i
+                  className=" ml-[20px] cursor-pointer text-teal-600 fa-solid fa-pen-to-square"
+                  onClick={() => setUpdateMode(true)}
+                />
+                <i
+                  className="ml-[20px] cursor-pointer text-red-400 fa-solid fa-trash"
+                  onClick={deleteHandler}
+                />
+              </div>
+            ) : null}
+          </h1>
+        )}
 
-        <h1 className=" text-center font-['Lora',serif] text-[28px]">
-          {post?.title}
-          {post?.username === user?.username ? (
-            <div className=" float-right text-[16px]">
-              <i className=" ml-[20px] cursor-pointer text-teal-600 fa-solid fa-pen-to-square" />
-              <i
-                className="ml-[20px] cursor-pointer text-red-400 fa-solid fa-trash"
-                onClick={deleteHandler}
-              />
-            </div>
-          ) : null}
-        </h1>
         <div className=" flex justify-between mb-[20px] text-[16px] text-[#b39656] font-['Varela Round',sans-serif]">
           <Link to={`/?username=${post.username}`} className="link">
             <div>
@@ -66,9 +100,38 @@ const SinglePost = () => {
           </Link>
           <div>{new Date(post.createdAt).toDateString()}</div>
         </div>
-        <p className=" font-['Josefin Sans',sans-serif] text-[#666] font-[18px] leading-[25px] first-letter:ml-[20px] first-letter:text-[30px]">
-          {post.desc}
-        </p>
+        {updateMode ? (
+          <textarea
+            value={desc}
+            rows="3"
+            className="w-[100%] font-['Lora',serif] text-[17px] focus:outline-none text-gray-600"
+            onChange={(e) => setDesc(e.target.value)}
+          />
+        ) : (
+          <p className=" font-['Josefin Sans',sans-serif] text-[#666] font-[18px] leading-[25px] first-letter:ml-[20px] first-letter:text-[30px]">
+            {desc}
+          </p>
+        )}
+        {updateMode ? (
+          <div className="flex justify-center">
+            <div className="px-[20px]">
+              <button
+                className="cursor-pointer bg-blue-400 text-white rounded-lg px-[10px] py-[5px] hover:bg-blue-500 "
+                onClick={updateHandler}
+              >
+                Update
+              </button>
+            </div>
+            <div className="px-[20px]">
+              <button
+                onClick={() => setUpdateMode(false)}
+                className="cursor-pointer bg-red-300 text-white rounded-lg px-[10px] py-[5px] hover:bg-red-400 "
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        ) : null}
       </div>
     </div>
   );
